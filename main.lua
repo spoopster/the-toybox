9f58c871f8c97fd940cca6ac4abbbebbb59f374a
spoopster <robyfiru70@gmail.com>
2025-01-13 08:04:59 +0200

    oops

--- main.lua
+++ main.lua
@@ -50,7 +50,7 @@ local mod_files = {
     "scripts.fortnite funnies.gold mode",
     "scripts.fortnite funnies.retro mode",
     --"scripts.fortnite funnies.yes",
-    "scripts.fortnite funnies.black_souls",
+    --"scripts.fortnite funnies.black_souls",
 
     --"scripts.test",
 }
